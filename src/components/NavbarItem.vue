358e0502684387df2771c5fc7cf930e7d37e3449
Gabriel <gabripavic@gmail.com>
2024-05-23 16:15:25 +0200

    Kostur za login i register

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,8 +1,8 @@
 <template>
     <fwb-navbar>
       <template #logo>
-        <fwb-navbar-logo alt="Flowbite logo" image-url="/images/logo.svg" link="#">
-          Flowbite
+        <fwb-navbar-logo alt="Flowbite logo" image-url="/stopwatch-icon.webp" link="#">
+          TimeStat
         </fwb-navbar-logo>
       </template>
       <template #default="{isShowMenu}">
@@ -11,13 +11,19 @@
             Home
           </fwb-navbar-link>
           <fwb-navbar-link link="#">
-            Services
+            Unos aktivnosti
           </fwb-navbar-link>
           <fwb-navbar-link link="#">
-            Pricing
+            Izracun aktivnosti
           </fwb-navbar-link>
           <fwb-navbar-link link="#">
-            Contact
+            Pregled aktivnosti
+          </fwb-navbar-link>
+          <fwb-navbar-link link="/login">
+            Login
+          </fwb-navbar-link>
+          <fwb-navbar-link link="/register">
+            Register
           </fwb-navbar-link>
         </fwb-navbar-collapse>
       </template>
---
+++ src/components/RegisterItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Pozdrav, sad si na stranici za register :)</h1>
+      <form @submit.prevent="login">
+        <input v-model="email" type="email" placeholder="Email" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+</template>
