3da5a33ccdbbd5e2339bc14464f8d3720928eb1f
Gabriel <gabripavic@gmail.com>
2024-05-24 02:17:55 +0200

    ikona aplikacije

--- src/App.vue
+++ src/App.vue
@@ -29,17 +29,20 @@ export default {
 <script>
 import { FwbDropdown, FwbListGroup, FwbListGroupItem } from 'flowbite-vue'
 import NavbarItem from './components/NavbarItem.vue'
+import FooterItem from './components/FooterItem.vue'
 export default {
   components: { 
     NavbarItem,
+    FooterItem,
   },
 }
 </script>
 
-<template>
+<template class="flex flex-col min-h-screen">
   <NavbarItem />
   <div>
-    <h1 class="mx-4 text-2xl text-center text-yellow-100 bg-cyan-600 ">Pozdrav Brate, ovo je zastava page</h1>
+    <h1 class="mx-4 text-2xl text-center text-yellow-100 bg-cyan-600 ">Pozdrav Brate, ovo je zastava</h1>
   </div>
   <router-view />
+  <FooterItem class="fixed w-full bg-cyan-300 mx-auto p-4 md:py-8 bottom-0" />
 </template>
---
+++ src/components/FooterItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <fwb-footer>
+      <fwb-footer-copyright
+        copyright-message=" Copyleft lololololol"
+      />
+    </fwb-footer>
+  </template>
+  
+  <script setup>
+  import {
+    FwbFooter,
+    FwbFooterCopyright,
+    FwbFooterLink,
+    FwbFooterLinkGroup,
+  } from 'flowbite-vue'
+  </script>
